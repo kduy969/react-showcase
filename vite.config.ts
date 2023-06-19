728da9f739eb77c97b7d003f4539c60cef7b9540
kduy969 <kduyit92@gmail.com>
2023-06-19 15:51:55 +0700

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-  },
-  base: "./",
+export default defineConfig((c) => {
+  const config = {
+    plugins: [react()],
+    server: {
+      host: true,
+    },
+    base: "/",
+  };
+
+  if (c.command !== "serve") {
+    config.base = "/react-showcase/";
+  }
+  return config;
 });
